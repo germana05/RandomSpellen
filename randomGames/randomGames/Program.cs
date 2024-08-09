268074596ab2710617335fa549d6985cf05e5a83
marshall verboekend <marshalllyron@gmail.com>
2024-08-09 14:13:12 +0200

    oke dan

---
+++ randomGames/randomGames/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace randomGames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
